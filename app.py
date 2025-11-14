4de74d5bf655f5512b55f14ec1563ecb38c56f3e
steven <steven@stevenalexander.org>
2025-11-13 19:49:06 -0500

    better route filtering

--- app.py
+++ app.py
@@ -7,28 +7,35 @@ app=Flask(__name__)
 app.config['CACHE_TYPE']='SimpleCache'
 cache=Cache(app)
 
-data=httpx.get("https://tarc.rideralerts.com/InfoPoint/rest/Routes/GetVisibleRoutes").json()
-routes=",".join(str(r.get("RouteId")) for r in data)
-data=httpx.get("https://tarc.rideralerts.com/InfoPoint/rest/Stops/GetAllStops").json()
-stops={s.get("StopId"):[[s.get("Latitude"),s.get("Longitude")],s.get("Name"),1 if s.get("IsTimePoint") else 0] for s in data}
+d=httpx.get("https://tarc.rideralerts.com/InfoPoint/rest/Routes/GetVisibleRoutes").json()
+routes=",".join(str(r.get("RouteId")) for r in d)
+stops={}
 
 @app.route("/")
 def index():
-    return render_template("map.html",stops=stops)
+    return render_template("map.html")
 
-@app.route("/tarc.csv")
+@app.route("/vehicles.csv")
 @cache.cached(timeout=5)
-def tarc():
+def vehicles():
     d=httpx.get("https://tarc.rideralerts.com/InfoPoint/rest/Vehicles/GetAllVehiclesForRoutes?routeIDs=0,"+routes).json()
     return "\n".join(f"{v['Latitude']},{v['Longitude']},{v['RouteId']},{v['Name']},{v['Destination']},{v['Heading']},{v['Speed']},{v['DirectionLong']},{v['LastUpdated'][6:16]}" for v in d)
+
+@app.route("/stops.json")
+@cache.cached(timeout=444)
+def allStops():
+    d=httpx.get("https://tarc.rideralerts.com/InfoPoint/rest/Stops/GetAllStops").json()
+    global stops
+    stops={s.get("StopId"):[[s.get("Latitude"),s.get("Longitude")],s.get("Name"),1 if s.get("IsTimePoint") else 0] for s in d}
+    return stops
 
 @app.route('/<int:stop>.csv')
 @cache.cached(timeout=5)
 def stop(stop):
     if stop not in stops:return "not a stop"
-    response=httpx.post("https://tarc.rideralerts.com/InfoPoint/Stops/Detail",json={"StopID":stop})
-    if response.status_code==200:
-        table=BeautifulSoup(response.content,"html.parser").find("table")
+    d=httpx.post("https://tarc.rideralerts.com/InfoPoint/Stops/Detail",json={"StopID":stop})
+    if d.status_code==200:
+        table=BeautifulSoup(d.content,"html.parser").find("table")
         if table:
             csv=[",".join([h.get_text(strip=True) for h in table.find_all("th")])]
             for row in table.find_all("tr")[1:]:
@@ -36,4 +43,10 @@ def stop(stop):
             return Response("\n".join(csv),mimetype="text/plain")
         else:
             return "no stop information"
-    return "fetch failed"
+    return "request failed"
+
+@app.route("/stops.<int:route>")
+@cache.cached(timeout=444)
+def stopsForRoute(route):
+    d=httpx.get("https://tarc.rideralerts.com/InfoPoint/rest/Stops/GetAllStopsForRoutes?routeIDs="+str(route)).json()
+    return ",".join(str(v['StopId']) for v in d)
